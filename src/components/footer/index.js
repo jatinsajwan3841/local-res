4df248aea80a08146ebf21e6d0533aa5f6bcf228
jatinsajwan3841 <jatinkumar@evolvfit.in>
2021-07-26 19:37:56 +0530

    footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            Tried making with care (❁´◡`❁), by : {` `}
+            <a className="nav-link" href="https://github.com/jatinsajwan3841">
+                jatin
+            </a>
+        </div>
+    )
+}
+
+export default Footer
