6ecba2fbd3974af4ae64fd2fed99422c0eceb0e7
jatinsajwan3841 <jatinsajwan3841@gmail.com>
2025-05-03 23:06:10 +0530

    Location update

--- src/components/constant/index.js
+++ src/components/constant/index.js
@@ -30,12 +30,12 @@ export const BRANCHES = [
 ]
 
 export const EXCELFILES = [
-    'dat/B. TECH. I SEM DEC 18.xlsx',
-    'dat/B. TECH. II SEM JUNE 2019.xlsx',
-    'dat/B. TECH. III SEM DECEMBER 2019.xlsx',
-    'dat/B. TECH. IV SEM DECEMBER 2020.xlsx',
-    'dat/B. TECH. V SEM DECEMBER 2020 (COVID19 ).xlsx',
-    'dat/B. TECH. VI SEM JUNE 2021 (COVID 19 ).xlsx',
-    'dat/B. TECH. VII SEM DECEMBER 2021 (ON LINE ).xlsx',
-    'dat/B. TECH. VIII SEM JUNE 2022.xlsx',
+    'data/B. TECH. I SEM DEC 18.xlsx',
+    'data/B. TECH. II SEM JUNE 2019.xlsx',
+    'data/B. TECH. III SEM DECEMBER 2019.xlsx',
+    'data/B. TECH. IV SEM DECEMBER 2020.xlsx',
+    'data/B. TECH. V SEM DECEMBER 2020 (COVID19 ).xlsx',
+    'data/B. TECH. VI SEM JUNE 2021 (COVID 19 ).xlsx',
+    'data/B. TECH. VII SEM DECEMBER 2021 (ON LINE ).xlsx',
+    'data/B. TECH. VIII SEM JUNE 2022.xlsx',
 ]
